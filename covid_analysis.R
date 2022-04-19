e80c31650dfba5cdafb54e1e817e8b22f993bcf6
rheaprag <54684793+rheaprag@users.noreply.github.com>
2022-04-18 21:55:27 -0500

    Update covid_analysis.R

--- covid_analysis.R
+++ covid_analysis.R
@@ -1,26 +1,11 @@
-#import csv files
-covid_vaccines <- read.csv('COVID-19_Vaccinations_in_the_United_States_Jurisdiction.csv')
-#important columns - use `colnames(covid_vaccines)`
-  #[3] - Location: gives state
-  #Vaccine Distributions
-    #[4] - General
-    #[5] - Janssen
-    #[6] - Moderna
-    #[7] - Pfizer
-  #Completed Series by Drug
-    #[34] - General
-    #[42] - Janssen
-    #[43] - Moderna
-    #[44] - Pfizer
-  #Additional Doses
-    #[58] - General?
-    #[70] - Janssen
-    #[68] - Moderna
-    #[69] - Pfizer
-  #Do we want to look by age?
+# Import Necessary CSV Files
 
+# The CDC Covid Vaccine CSV was extracted from the original CSV found in the READMe file. 
+covid_vaccines <- read.csv('covid_vac.csv')
+# The NYT Covid CSV File was unable to be downloaded, so it was pulled directly from the github. 
+  # This also ensures that the data is the most recent as the github repository is regularly updated.
 nyt_covid <- read.csv("https://github.com/nytimes/covid-19-data/raw/master/us-states.csv")
-#Covid cases and deaths listed by state
+  #Covid-19 cases and deaths are listed by state
   
 #Because the nyt_covid file lists states in its full name vs abbreviation like the covid_vaccines file...
 nyt_covid$state <- state.abb[match(nyt_covid$state, state.name)]
