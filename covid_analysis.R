32f337a45a46578b551f21c899240eea71f34175
rheaprag <54684793+rheaprag@users.noreply.github.com>
2022-04-20 12:25:00 -0500

    Update covid_analysis.R

--- covid_analysis.R
+++ covid_analysis.R
@@ -12,49 +12,31 @@ nyt_covid$state <- state.abb[match(nyt_covid$state, state.name)]
   #What do do about NAs? I think manually adding them would work bc I think there are abbreviations in the covid_vaccines file for them?
     #or we could just remove them? but there are 4026 of them
 
+# Pulls most recent covid rates (4/17/22)
+nyt_cov <- nyt_covid[ c(42847:42902), ]
+
 # The nyt_covid file also has a different date format than the covid_vaccines file (not sure if we want to use date)
 nyt_covid$newdate <- strptime(as.character(nyt_covid$date), "%Y-%m-%d")
 nyt_covid$newdate <- format(nyt_covid$newdate, "%m/%d/%Y")
 
-#Merge datasets individually by state
+#Merge covid vaccine dataset by state
 cov_vac <- aggregate(x = covid_vaccines, by = list(covid_vaccines$Location), FUN = function(x) na.omit(x)[1])[,-1]
-nyt_cov <- aggregate(x = nyt_covid, by = list(nyt_covid$state), FUN = function(x) na.omit(x)[1])[,-1]
-
-# Pulls most recent covid rates (4/17/22)
-nyt_cov <- nyt_covid[ c(42847:42902), ]
 
 # Merge the datasets
 data <- merge(nyt_cov, cov_vac, by.x="state", by.y="Location")
-
-                     
-#####
-#changes
-
-
-#did you mean to use the original covid_vaccines file rather than the one shortened using Python?
-#I switched it to the shortened file here
-
-#Merge datasets individually by state
-cov_vac <- aggregate(x = cov_vac, by = list(cov_vac$Location), FUN = function(x) na.omit(x)[1])[,-1]
-nyt_cov <- aggregate(x = nyt_covid, by = list(nyt_covid$state), FUN = function(x) na.omit(x)[1])[,-1]
-
-#whats the fip column?
-# Pulls most recent covid rates (4/17/22)
-nyt_cov <- nyt_covid[ c(42847:42902), ]
-
-
-# Merge the datasets
-data <- merge(nyt_cov, cov_vac, by.x="state", by.y="Location")
-
-View(data)
 
 #Predict number of cases in any given state using all predictors
 all_pred_lin_model <- lm(cases ~ Distributed + Distributed_Janssen + Distributed_Moderna + Distributed_Pfizer + Series_Complete_Yes + 
                 Series_Complete_Janssen + Series_Complete_Moderna + Series_Complete_Pfizer + Additional_Doses + Additional_Doses_Janssen +
                 Additional_Doses_Moderna + Additional_Doses_Pfizer, data=data)
-
+                     
+  #LM returns with NA for Distributed_Pfizer...why?
+  #When I run LM without Distributed_Moderna, I get result for Pfizer?
+      #all_pred_lin_model <- lm(cases ~ Distributed + Distributed_Janssen + Distributed_Pfizer + Series_Complete_Yes + Series_Complete_Janssen +
+                     #Series_Complete_Moderna + Series_Complete_Pfizer + Additional_Doses + Additional_Doses_Janssen +
+                     #Additional_Doses_Moderna + Additional_Doses_Pfizer, data=data)
+                     
 #Summarize model and see significance of predictors
-#Distributed_Pfizer is the most significant
 summary(all_pred_lin_model)
                      
 #I was thinking we can make a bunch of models and compare
