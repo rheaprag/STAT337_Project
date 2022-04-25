2905855aecd0c2daf81d397cd5350572ccecd811
rheaprag <54684793+rheaprag@users.noreply.github.com>
2022-04-25 11:13:34 -0500

    Update covid_analysis.R

--- covid_analysis.R
+++ covid_analysis.R
@@ -2,28 +2,24 @@
 
 # The CDC Covid Vaccine CSV was extracted from the original CSV found in the READMe file. 
 covid_vaccines <- read.csv('covid_vac.csv')
+# Pulls most recent covid rates (4/17/22)
+covid_vac <- covid_vaccines[ c(449:512), ]
+
 # The NYT Covid CSV File was unable to be downloaded, so it was pulled directly from the github. 
-  # This also ensures that the data is the most recent as the github repository is regularly updated.
+# This also ensures that the data is the most recent as the github repository is regularly updated.
 nyt_covid <- read.csv("https://github.com/nytimes/covid-19-data/raw/master/us-states.csv")
-  #Covid-19 cases and deaths are listed by state
-  
-#Because the nyt_covid file lists states in its full name vs abbreviation like the covid_vaccines file...
-nyt_covid$state <- state.abb[match(nyt_covid$state, state.name)]
-  #What do do about NAs? I think manually adding them would work bc I think there are abbreviations in the covid_vaccines file for them?
-    #or we could just remove them? but there are 4026 of them
+#Covid-19 cases and deaths are listed by state
 
 # Pulls most recent covid rates (4/17/22)
 nyt_cov <- nyt_covid[ c(42847:42902), ]
 
-# The nyt_covid file also has a different date format than the covid_vaccines file (not sure if we want to use date)
-nyt_covid$newdate <- strptime(as.character(nyt_covid$date), "%Y-%m-%d")
-nyt_covid$newdate <- format(nyt_covid$newdate, "%m/%d/%Y")
-
-#Merge covid vaccine dataset by state
-cov_vac <- aggregate(x = covid_vaccines, by = list(covid_vaccines$Location), FUN = function(x) na.omit(x)[1])[,-1]
+#Because the nyt_cov file lists states in its full name vs abbreviation like the covid_vaccines file...
+nyt_cov$state <- state.abb[match(nyt_cov$state, state.name)]
+#What do do about NAs? I think manually adding them would work bc I think there are abbreviations in the covid_vaccines file for them?
+#or we could just remove them? but there are 4026 of them
 
 # Merge the datasets
-data <- merge(nyt_cov, cov_vac, by.x="state", by.y="Location")
+data <- merge(nyt_cov, covid_vac, by.x="state", by.y="Location")
 
 #Predict number of cases in any given state using all predictors
 #Removing predictor 'Distributed' so redudancy between the Distrubted variables is no longer an issue
