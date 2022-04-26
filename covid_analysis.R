9e4b30988df4fa8ffad0d83ec8697c1cc49642f5
rheaprag <54684793+rheaprag@users.noreply.github.com>
2022-04-26 10:28:14 -0500

    Update covid_analysis.R

--- covid_analysis.R
+++ covid_analysis.R
@@ -12,14 +12,12 @@ nyt_covid <- read.csv("https://github.com/nytimes/covid-19-data/raw/master/us-st
 # Pulls most recent covid rates (4/17/22)
 nyt_cov <- nyt_covid[ c(42847:42902), ]
 
-#Because the nyt_cov file lists states in its full name vs abbreviation like the covid_vaccines file...
+# Because the nyt_cov file lists states in its full name vs abbreviation like the covid_vaccines file...
 nyt_cov$state <- state.abb[match(nyt_cov$state, state.name)]
-#What do do about NAs? I think manually adding them would work bc I think there are abbreviations in the covid_vaccines file for them?
-#or we could just remove them? but there are 4026 of them
 
-#The Population CSV was downloaded and extracted from the original
+# The Population CSV was downloaded and extracted from the original
 pop <- read.csv("pop.csv")
-#Because the pop file lists states in its full name vs abbreviation like the covid_vaccines file...
+# Because the pop file lists states in its full name vs abbreviation like the covid_vaccines file...
 pop$Name <- state.abb[match(pop$Name, state.name)]
 
 # Merge the datasets
@@ -27,32 +25,32 @@ nyt_and_covid <- merge(nyt_cov, covid_vac, by.x="state", by.y="Location")
 data <- merge(nyt_and_covid, pop, by.x="state", by.y="Name")
 
 ########################Linear Regression Model########################
-#Predict number of cases in any given state using all predictors
-#Removing predictor 'Distributed' so redudancy between the Distrubted variables is no longer an issue
+# Predict number of cases in any given state using all predictors
+# Removing predictor 'Distributed' so redudancy between the Distrubted variables is no longer an issue
 all_pred_lin_model <- lm(cases ~ Distributed_Janssen + Distributed_Moderna + Distributed_Pfizer + Series_Complete_Yes + 
                            Series_Complete_Janssen + Series_Complete_Moderna + Series_Complete_Pfizer + Additional_Doses + Additional_Doses_Janssen +
                            Additional_Doses_Moderna + Additional_Doses_Pfizer + Pop_2020, data=data)
 
 ########################Backward Selection########################
-#Remove 'Series_Complete_Moderna'
+# Remove 'Series_Complete_Moderna'
 all_pred_lin_model <- lm(cases ~ Distributed_Janssen + Distributed_Moderna + Distributed_Pfizer + Series_Complete_Yes + 
                            Series_Complete_Janssen + Series_Complete_Pfizer + Additional_Doses + Additional_Doses_Janssen +
                            Additional_Doses_Moderna + Additional_Doses_Pfizer + Pop_2020, data=data)
-#Remove 'Distributed_Janssen'
+# Remove 'Distributed_Janssen'
 all_pred_lin_model <- lm(cases ~ Distributed_Moderna + Distributed_Pfizer + Series_Complete_Yes + 
                            Series_Complete_Janssen + Series_Complete_Pfizer + Additional_Doses + Additional_Doses_Janssen +
                            Additional_Doses_Moderna + Additional_Doses_Pfizer + Pop_2020, data=data)
-#Remove 'Series_Complete_Janssen'
+# Remove 'Series_Complete_Janssen'
 all_pred_lin_model <- lm(cases ~ Distributed_Moderna + Distributed_Pfizer + Series_Complete_Yes + Series_Complete_Pfizer + Additional_Doses + 
                            Additional_Doses_Janssen + Additional_Doses_Moderna + Additional_Doses_Pfizer + Pop_2020, data=data)
-#Remove 'Series_Complete_Pfizer'
+# Remove 'Series_Complete_Pfizer'
 all_pred_lin_model <- lm(cases ~ Distributed_Moderna + Distributed_Pfizer + Series_Complete_Yes + Additional_Doses + 
                            Additional_Doses_Janssen + Additional_Doses_Moderna + Additional_Doses_Pfizer + Pop_2020, data=data)
-#Remove 'Distributed_Moderna'
+# Remove 'Distributed_Moderna'
 all_pred_lin_model <- lm(cases ~ Distributed_Pfizer + Series_Complete_Yes + Additional_Doses + 
                            Additional_Doses_Janssen + Additional_Doses_Moderna + Additional_Doses_Pfizer + Pop_2020, data=data)
 
-#Summarize model and see significance of predictors
+# Summarize model and see significance of predictors
 summary(all_pred_lin_model)
 
 # Plots
@@ -65,48 +63,48 @@ abline(data_lm)
 gen_lin_model <- lm(cases ~ Distributed + Series_Complete_Yes + 
                       Additional_Doses, data=data)
 
-#view summary statistics
+# Summary statistics
 summary(gen_lin_model)
-#Additional doses is by far the strongest predictor
-#Adjusted R-squared:  0.9802 
+# Additional doses is by far the strongest predictor
+# Adjusted R-squared:  0.9802 
 
 
 # Janssen only model
 janssen_lin_model <- lm(cases ~ Distributed_Janssen + Series_Complete_Janssen + 
                           Additional_Doses_Janssen + Pop_2020, data=data)
 
-#summary statistics
+# Summary statistics
 summary(janssen_lin_model)
-#Distributed_Janssen is the strongest predictor
-#Adjusted R-squared:  0.9734 
+# Distributed_Janssen is the strongest predictor
+# Adjusted R-squared:  0.9734 
 
 
 # Moderna only model
 moderna_lin_model <- lm(cases ~ Distributed_Moderna + Series_Complete_Moderna + 
                           Additional_Doses_Moderna + Pop_2020, data=data)
 
-#summary statistics
+# Summary statistics
 summary(moderna_lin_model)
-#Additional_Doses and Series_Complete
-#Adjusted R-squared:  0.9831
+# Additional_Doses and Series_Complete
+# Adjusted R-squared:  0.9831
 
 # Pfizer only model
 pfizer_lin_model <- lm(cases ~ Distributed_Pfizer + Series_Complete_Pfizer + 
                          Additional_Doses_Pfizer + Pop_2020, data=data)
 
-#summary statistics
+# Summary statistics
 summary(pfizer_lin_model)
-#Additional doses is best predictor
-#Adjusted R-squared:  0.9759 
+# Additional doses is best predictor
+# Adjusted R-squared:  0.9759 
 
-#Try quadratic and other types of predictors? 
-#Research herd immunity and more background info so we have justification for choice of predictors
+# Try quadratic and other types of predictors? 
+# Research herd immunity and more background info so we have justification for choice of predictors
 
 ########################Old Data########################
-#Data from 4/17/21
+# Data from 4/17/21
 nyt_cov_old <- nyt_covid[ c(22565:22619), ]
 cov_vac_old <- covid_vaccines[ c(22780:22844), ]
-#Repeat previous necessary steps
+# Repeat previous necessary steps
 nyt_cov_old$state <- state.abb[match(nyt_cov_old$state, state.name)]
 data_old <- merge(nyt_cov_old, cov_vac_old, by.x="state", by.y="Location")  
 
@@ -170,18 +168,16 @@ for(index.test in index.fold){
 # Plot the 5 MSEs
 plot(1:5, MSEs, type='b', col='red', xlab='Fold', ylab='MSE')
 
-####
-
-# backward selection based on AIC (same results)
+########################AIC Backward Selection########################
+# Backward selection based on AIC 
 step(all_pred_lin_model, direction = 'backward')
+# Returns the same results as the backward selection seen above
 
-
-#######################################bootstrap
-
-#empty vector for bootstrap estimations
+########################Bootstrapping########################
+# Create an empty vector for bootstrap estimations
 dist_pfizer.hat <- c()
 
-#resample 100 times
+# Resample 100 times
 set.seed(2022)
 
 for(i in 1:100){
@@ -193,22 +189,20 @@ for(i in 1:100){
                              Additional_Doses_Pfizer + Pop_2020, data=data.boot)
   
   dist_pfizer.hat[i] <- all_pred_lin_model$coefficients[2]
-  
 }
+
 dist_pfizer.hat
 hist(dist_pfizer.hat)
-
 
 all_pred_lin_model <- lm(cases ~ Distributed_Janssen + Distributed_Moderna + Distributed_Pfizer + Series_Complete_Yes + 
                            Series_Complete_Janssen + Series_Complete_Moderna + Series_Complete_Pfizer + Additional_Doses + Additional_Doses_Janssen +
                            Additional_Doses_Moderna + Additional_Doses_Pfizer + Pop_2020, data=data)
 
-####
-
-#empty vector for bootstrap estimations
+#####
+# Create an empty vector for bootstrap estimations
 dist_series_complete.hat <- c()
 
-#resample 100 times
+# Resample 100 times
 set.seed(2022)
 
 for(i in 1:100){
@@ -220,17 +214,15 @@ for(i in 1:100){
                              Additional_Doses_Pfizer + Pop_2020, data=data.boot)
   
   dist_series_complete.hat[i] <- all_pred_lin_model$coefficients[3]
-  
 }
 
 hist(dist_series_complete.hat)
 
 #####
-
-#empty vector for bootstrap estimations
+# Create an empty vector for bootstrap estimations
 dist_add_doses.hat <- c()
 
-#resample 100 times
+# Resample 100 times
 set.seed(2022)
 
 for(i in 1:100){
@@ -242,18 +234,15 @@ for(i in 1:100){
                              Additional_Doses_Pfizer + Pop_2020, data=data.boot)
   
   dist_add_doses.hat[i] <- all_pred_lin_model$coefficients[4]
-  
 }
 
 hist(dist_add_doses.hat)
 
-
 #####
-
-#empty vector for bootstrap estimations
+# Create an empty vector for bootstrap estimations
 dist_add_janssen.hat <- c()
 
-#resample 100 times
+# Resample 100 times
 set.seed(2022)
 
 for(i in 1:100){
@@ -265,18 +254,15 @@ for(i in 1:100){
                              Additional_Doses_Pfizer + Pop_2020, data=data.boot)
   
   dist_add_janssen.hat[i] <- all_pred_lin_model$coefficients[5]
-  
 }
 
 hist(dist_add_janssen.hat)
 
-
 #####
-
-#empty vector for bootstrap estimations
+# Create an empty vector for bootstrap estimations
 dist_add_moderna.hat <- c()
 
-#resample 100 times
+# Resample 100 times
 set.seed(2022)
 
 for(i in 1:100){
@@ -288,17 +274,15 @@ for(i in 1:100){
                              Additional_Doses_Pfizer + Pop_2020, data=data.boot)
   
   dist_add_moderna.hat[i] <- all_pred_lin_model$coefficients[6]
-  
 }
 
 hist(dist_add_moderna.hat)
 
 #####
-
-#empty vector for bootstrap estimations
+# Create an empty vector for bootstrap estimations
 dist_add_pfizer.hat <- c()
 
-#resample 100 times
+# Resample 100 times
 set.seed(2022)
 
 for(i in 1:100){
@@ -310,20 +294,15 @@ for(i in 1:100){
                              Additional_Doses_Pfizer + Pop_2020, data=data.boot)
   
   dist_add_pfizer.hat[i] <- all_pred_lin_model$coefficients[7]
-  
 }
 
 hist(dist_add_pfizer.hat)
 
-
 #####
-
-
-
-#empty vector for bootstrap estimations
+# Create an empty vector for bootstrap estimations
 dist_pop.hat <- c()
 
-#resample 100 times
+# Resample 100 times
 set.seed(2022)
 
 for(i in 1:100){
@@ -335,10 +314,6 @@ for(i in 1:100){
                              Additional_Doses_Pfizer + Pop_2020, data=data.boot)
   
   dist_pop.hat[i] <- all_pred_lin_model$coefficients[8]
-  
 }
 
 hist(dist_pop.hat)
-
-
-#####
